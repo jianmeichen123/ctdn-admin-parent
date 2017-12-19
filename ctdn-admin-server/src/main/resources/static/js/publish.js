9eeb3f7cb74fa4a97cf75d6ff584e10b4c4ed7b7
wanghao <haonanwang@galaxyinternet.com>
2017-12-19 19:45:35 +0800

    publish

--- ctdn-admin-server/src/main/resources/static/js/publish.js
+++ ctdn-admin-server/src/main/resources/static/js/publish.js
@@ -1,39 +1,52 @@
 $(function(){
-   $('.publish-report-span').click(function(){
-        var url ="insertReport"
-        var data = JSON.stringify($("#form").serializeJson());
-        $.ajax({
-            type: "POST",
-            url:url,
-            contentType:'application/json;charset=UTF-8',
-            data:data,
-            success:function(data){
-                if(data.message == 'OK')
-                {
-                    layer.msg("保存成功,我们会尽快联系您~",0,function(){
-                        location.href="index"
-                    });
-
+        //表单清空
+       $("#form")[0].reset();
+            //表单验证
+        $("#form").validate({
+            submitHandler: function() {
+                var title = $("input[name='title']").val();
+                if(!title){
+                    $(".publish-title").addClass("red")
+                    return;
                 }
-                else
-                {
-                    layer.msg("保存失败,请稍后再试!");
-                }
+             save();
             }
         })
-
-//        var title=JSON.stringify("")
-//        $.post("insertReport",{"title":title},function(data){})
+   $('.publish-report').click(function(){
+        $("#form").submit();
    })
 })
 
 $.fn.serializeJson = function(){
 		var data = {};
 		var array = this.serializeArray();
+		data["state"]=0;
 		$.each(array,function(){
 			data[this.name]=this.value.replace(/(^\s+)|(\s+$)/g,"");
 		})
 		return data;
 	}
 
+function save(){
+    var url ="insertReport"
+    var data = JSON.stringify($("#form").serializeJson());
+    $.ajax({
+        type: "POST",
+        url:url,
+        contentType:'application/json;charset=UTF-8',
+        data:data,
+        success:function(data){
+            if(data.message == 'OK')
+            {
+                layer.msg("保存成功~",{time:1000},function(data){
+                    location.href="index"
+                });
+            }
+            else
+            {
+                layer.msg("保存失败~~!");
+            }
+        }
+    })
+}
 
